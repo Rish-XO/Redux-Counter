45e4342413ef5842210b41ae701e352a71eb04c0
Rishal <Rishal.mhd009@gmail.com>
2023-05-28 11:53:25 +0530

    multi state implemented

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,28 +1,31 @@
-import classes from './Counter.module.css';
-import {useSelector, useDispatch} from 'react-redux'
+import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
   const dispatch = useDispatch();
-const counter = useSelector(state => state.counter)
+  const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
-const incrementHandler = () => {
-  dispatch({type : "increment"})
-}
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
 
-const increaseHandler = () => {
-  dispatch({type : "increase", amount: 10})
-}
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: 10 });
+  };
 
-const decrementHandler = () => {
-  dispatch({type: 'decrement'})
-}
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increaseHandler}>Increase by 10</button>
